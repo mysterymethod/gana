ded1a27fb81e0009c1a9fb4428520ee0500ddddf
pranoy <chandra.pranay@yahoo.in>
2022-10-11 15:50:53 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// Faster react create app alternative.
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
