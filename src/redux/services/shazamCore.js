8c9a592058482c5802f35fe99103757e4d88b4de
pranoy <chandra.pranay@yahoo.in>
2022-10-13 15:10:11 +0530

    shazam core setup complete

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,3 +1,5 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
 const options = {
   method: 'GET',
   headers: {
@@ -10,3 +12,15 @@ fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key','ffc4bdef7bmsh008ba9208359439p1d4f45jsn848dea47c83d')
+
+      return headers
+    }
+  })
+})
